adcddaa4613593662d35901248d8f816d4257baa
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-12-11 15:40:12 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,60 @@
+# ЦИКЛ WHILE
+
+# i = 0 
+# while i <=10:
+#     if i % 2 == 1: break
+#     print(i)
+#     i+=1
+# if True:
+#     pass 
+# while int(input('Enter number:')) > 0: pass
+
+# for i in range(10000000):
+    # if int(input('Enter number:')) < 0: break
+
+# a = int(input('Enter 1st number:'))
+# b = int(input('Enter 2nd number:'))
+
+# if b == 0:
+#     i = 1
+#     while True:
+#         # Если пользователь ввел нуль больше 3-х раз
+#         if i > 3:
+#             print('Stupid asshole')
+#         else: print('aaaaaa!')
+#         b = int(input('Enter 2nd number:'))
+#         if b != 0:
+#                 print(a / b)
+#                 break
+#         i+=1
+# else:
+#     print(a / b)
+
+# a = int(input('Enter 1st number:'))
+# i = 1
+# while True:
+#     try:
+#         b = int(input('Enter 2nd number:'))
+#         print(a / b)
+#         break
+#     except:
+#         if i > 3: print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA! You are stupid!')
+#         else: print('Please enter non-zero value')
+#         i += 1
+# while True:
+#     a = int(input('Enter positive number:'))
+#     if a > 0:break
+#     else: print('You were asked to enter positive number')
+
+# while True:
+#     try:
+#         a = int(input('Enter positive number:'))
+#         if a < 0: raise ValueError('xexexexexexe')
+#         else:break
+#     except ValueError as ve:
+#         print(ve)
+
+
+
+
+
